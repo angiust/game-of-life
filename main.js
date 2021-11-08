772c2b122b35668a20fd4d6cc4129a995e334062
angiust <angelo.giustiniani@gmail.com>
2021-11-08 01:24:31 +0100

    refactoring main.js

--- main.js
+++ main.js
@@ -10,28 +10,37 @@ function addToggledCell(row) {
   cell.addEventListener('click', myFunc, false);
 }
 
+function addMissingRows(nrow, griglia) {
+  while (nrow > griglia.rows.length) {
+    var row = griglia.insertRow(-1);
+    for (var i = 0; i < griglia.rows[0].cells.length; i++)
+      addToggledCell(row);
+  }
+}
+
+function deleteExcessRows(nrow, griglia) {
+  while (nrow < griglia.rows.length)
+    griglia.deleteRow(-1);
+}
+
+function addMissingCols(ncol, griglia) {
+  while (ncol > griglia.rows[0].cells.length)
+    for (var i = 0; i < griglia.rows.length; i++)
+      addToggledCell(griglia.rows[i]);
+}
+
+function deleteExcessCols(ncol, griglia) {
+  while (ncol < griglia.rows[0].cells.length)
+    for (var i = 0; i < griglia.rows.length; i++)
+      griglia.rows[i].deleteCell(-1);
+}
+
 function updateRowsColumns() {
   var nrow = document.getElementById('row').value;
   var ncol = document.getElementById('col').value;
   var griglia = document.getElementById('griglia');
-  while (nrow > griglia.rows.length) {
-      var row = griglia.insertRow(-1);
-      for (var i = 0; i < griglia.rows[0].cells.length; i++) {
-        addToggledCell(row);
-      }
-  }
-  while (nrow < griglia.rows.length) {
-      griglia.deleteRow(-1);
-  }
-  while (ncol > griglia.rows[0].cells.length) {
-    for (var i = 0; i < griglia.rows.length; i++) {
-      addToggledCell(griglia.rows[i]);
-    }
-
-  }
-  while (ncol < griglia.rows[0].cells.length) {
-    for (var i = 0; i < griglia.rows.length; i++) {
-      griglia.rows[i].deleteCell(-1);
-    }
-  }
+  addMissingRows(nrow, griglia);
+  deleteExcessRows(nrow, griglia);
+  addMissingCols(ncol, griglia);
+  deleteExcessCols(ncol, griglia);
 }
